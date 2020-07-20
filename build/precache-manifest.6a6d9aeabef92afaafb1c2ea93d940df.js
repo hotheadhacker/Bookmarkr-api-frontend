088139e58e08ea26d2f5dafbbd1e571791e542dd
hotheadhacker <isalmanqureshi@gmail.com>
2020-07-20 20:21:51 +0530

    uploaded buildd

---
+++ build/precache-manifest.6a6d9aeabef92afaafb1c2ea93d940df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6db800854c4c5376d006dc692f216b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e85f592f61c7410c1fcd",
+    "url": "/static/js/2.1dbbb8eb.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.1dbbb8eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec10a626e4898e5f37b",
+    "url": "/static/js/main.b4ba4db6.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
